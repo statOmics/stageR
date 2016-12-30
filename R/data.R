57d0b4c1148847331ae4f56b569185b627cb950f
koenvandenberge <koen.vdberge@gmail.com>
2016-12-30 11:50:58 +0100

    document esetProstate dataset

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
-#' #' Transcript-level abundance estimates in 14 Chinese prostate cancer patients
-#' #'
-#' #' A dataset containing 14 matched samples of tumoral prostate cancer and normal tissue, both derived from the same Chinese patient.
-#' #'
-#' #' @format A data frame with 173259 rows and 28 variables.
-#' #' @source \url{http://pachterlab.github.io/lair/}
-#' "prostateData"
+#' Transcript-level abundance estimates in 14 Chinese prostate cancer patients
+#'
+#' A dataset containing 14 matched samples of tumoral prostate cancer and normal tissue, both derived from the same Chinese patient. The dataset has been prefiltered to reduce the computational burden of the vignette.
+#'
+#' @format An \code{\link{ExpressionSet}} object.
+#' @source \url{http://pachterlab.github.io/lair/}
+"esetProstate"
